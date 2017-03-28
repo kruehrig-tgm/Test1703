48f6c363277a2d49be83f4d063c329278419615f
kruehrig <kruehrig@student.tgm.ac.at>
2017-03-28 16:18:04 +0200

    Test1703 erstellt

---
+++ src/kevin/Test1703.java
@@ -0,0 +1,14 @@
+package kevin;
+
+public class Test1703 {
+
+	public Test1703() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hallo Kevin!");
+	}
+
+}
