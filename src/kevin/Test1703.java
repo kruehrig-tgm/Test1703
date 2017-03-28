a07a54745636aaace021aaa814ff78a63a22baaf
kruehrig <kruehrig@student.tgm.ac.at>
2017-03-28 16:20:41 +0200

    Mehrere Ausgaben (Version 2)

--- src/kevin/Test1703.java
+++ src/kevin/Test1703.java
@@ -9,6 +9,8 @@ public class Test1703 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hallo Kevin!");
+		System.out.println("Hallo Kevin!");
+		System.out.println("Hallo Kevin!");
 	}
 
 }
