b02ff86de14954c19f2094bd583f1a39c3581d64
kruehrig <kruehrig@student.tgm.ac.at>
2017-03-28 16:24:33 +0200

    Neuer Branch "smiley" hinzugefügt. Rufzeichen durch Smiley ersetzt.

--- src/kevin/Test1703.java
+++ src/kevin/Test1703.java
@@ -8,7 +8,7 @@ public class Test1703 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo Kevin!");
+		System.out.println("Hallo Kevin :)");
 	}
 
 }
